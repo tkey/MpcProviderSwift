af7c9e43b66e4666f946e642bc5762da0f2090fb
ieow <cwlee0@gmail.com>
2024-12-11 15:34:53 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,9 +16,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/argentlabs/web3.swift", from:"1.6.0"),
         .package(url: "https://github.com/tkey/curvelib.swift", exact: "1.0.1"),
-//        .package(url: "https://github.com/tkey/mpc-core-kit-swift", from: "1.0.0-alpha"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
-        .package(url: "https://github.com/tkey/mpc-core-kit-swift", branch: "main"),
+        .package(url: "https://github.com/tkey/mpc-core-kit-swift", branch: "update_dependencies"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
